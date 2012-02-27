2a3f910a5876fd0b770eb84589d35ab16c238272
Tiago Floriano <mail@paico.com.br>
2012-02-27 14:10:10 -0300

    Inicio do repositório.

---
+++ usuario/painel.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo mv /usr/bin/gnome-panel /usr/bin/gnome-panel-old
+cd ~/Área\ de\ Trabalho/
+mkdir icon
+cd icon
+wget http://172.16.1.156:7070/icones.tar.gz
+tar -vzxf icones.tar.gz
+mv * ../
+cd ../
+rm -rf icon
+rm -rf firefox.desktop
+rm -rf google-chrome.desktop
+rm -rf icones.tar.gz
+chmod +x *.sh
+rm -rf painel.sh
+ip="$(ifconfig eth0 | grep "end.:" | cut -f2 -d':' | sed 's/Bcast/ /g' | sed 's/ //g')"
+gnome-open http://172.16.1.156:7070/upgrade.php?ip=$ip
+/usr/lib/indicator-session/gtk-logout-helper --logout
